dcd3aa9772e5c34f386c18d09d99c3ab793e74df
Andy Coupe <andrewmcoupe@gmail.com>
2026-05-06 11:26:28 +0100

    Allow releasing (#3)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -56,6 +56,8 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .setup(move |app| {
             let conn = db::init().expect("failed to initialize global database");
             app.manage(GlobalDb(Mutex::new(conn)));
